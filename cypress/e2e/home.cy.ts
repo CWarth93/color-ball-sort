e3e96a094a54c44d4e649ccaa39e9acc57706767
chwa1679 <christopher.warth@web.de>
2026-06-04 17:11:35 +0200

    Copy imprint content from 3dprompt

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -5,4 +5,14 @@ describe('home page', () => {
 		cy.get('[data-testid="game-board"]').should('be.visible');
 		cy.contains('a', 'Imprint').should('be.visible');
 	});
+
+	it('shows the copied imprint content', () => {
+		cy.visit('/imprint');
+		cy.contains('h1', 'Impressum').should('be.visible');
+		cy.contains('Christopher Warth').should('be.visible');
+		cy.contains('Ziegelstra').should('be.visible');
+		cy.contains('christopher.warth@web.de').should('have.attr', 'href', 'mailto:christopher.warth@web.de');
+		cy.contains('Impressum Generator von JuraForum.de').should('have.attr', 'href', 'https://www.juraforum.de/impressum-generator/');
+		cy.contains('a', 'Back to game').should('have.attr', 'href', '/');
+	});
 });
